4f9f0526a55158e0b7f5d3baba05189230608d6c
haydenz <40226873+haydenz@users.noreply.github.com>
2020-11-24 15:07:08 +0800

    Update phone_book.py

--- data-structures/week3_hash_tables/1_phone_book/phone_book.py
+++ data-structures/week3_hash_tables/1_phone_book/phone_book.py
@@ -13,33 +13,44 @@ def read_queries():
 
 def write_responses(result):
     print('\n'.join(result))
+    # print(result)
 
 def process_queries(queries):
     result = []
     # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = []
+    contacts = [-1] * 10000000
     for cur_query in queries:
         if cur_query.type == 'add':
             # if we already have contact with such number,
             # we should rewrite contact's name
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
-            else: # otherwise, just add it
-                contacts.append(cur_query)
+            if cur_query.name == contacts[cur_query.number]:
+                break
+            else:
+                contacts[cur_query.number] = cur_query.name
+            # for contact in contacts:
+            #     if contact.number == cur_query.number:
+            #         contact.name = cur_query.name
+            #         break
+            # else: # otherwise, just add it
+            #     contacts.append(cur_query)
         elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
+            if contacts[cur_query.number] != -1:
+                contacts[cur_query.number] = -1
+            # for j in range(len(contacts)):
+            #     if contacts[j].number == cur_query.number:
+            #         contacts.pop(j)
+            #         break
         else: # cur_query.type == 'find'
             response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
-            result.append(response)
+            if contacts[cur_query.number] == -1:
+                result.append(response)
+            else:
+                result.append(contacts[cur_query.number])
+            # for contact in contacts:
+            #     if contact.number == cur_query.number:
+            #         response = contact.name
+            #         break
+            # result.append(response)
     return result
 
 if __name__ == '__main__':
