0d167d1ef39d126f1f9f7f48727f4278ec19709f
haydenz <40226873+haydenz@users.noreply.github.com>
2020-12-05 00:03:43 +0800

    Update substring_equality.py

--- data-structures/week3_hash_tables/4_substring_equality/substring_equality.py
+++ data-structures/week3_hash_tables/4_substring_equality/substring_equality.py
@@ -3,10 +3,31 @@
 import sys
 
 class Solver:
-	def __init__(self, s):
-		self.s = s
+	_multiplier = 5
+	_prime = 1000003
+
+	def __init__(self, text):
+		self.text = text
+		self.text_len = len(text)
+		self.hash_l = None
+
 	def ask(self, a, b, l):
-		return s[a:a+l] == s[b:b+l]
+		# return self.text[a:a+l] == self.text[b:b+l]
+		poly = 1
+		for i in range(1, l + 1):
+			poly = (poly * self._multiplier) % self._prime
+		return self.fast_hash(a, l, poly) == self.fast_hash(b, l, poly)
+	
+	def fast_hash(self, a, l, poly):
+		hash1 = self.poly_hash(self.text[:a+l])
+		hash2 = self.poly_hash(self.text[:a])
+		return hash1-poly*hash2
+   
+	def poly_hash(self, s):
+		ans = 0
+		for c in reversed(s):
+			ans = (ans * self._multiplier + ord(c)) % self._prime # ord() returns Unicode char
+		return ans
 
 s = sys.stdin.readline()
 q = int(sys.stdin.readline())
