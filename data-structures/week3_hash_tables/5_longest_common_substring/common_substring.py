fffd230362844a528ab5986abb534bccda49ff52
haydenz <40226873+haydenz@users.noreply.github.com>
2020-12-12 11:45:56 +0800

    Update common_substring.py

--- data-structures/week3_hash_tables/5_longest_common_substring/common_substring.py
+++ data-structures/week3_hash_tables/5_longest_common_substring/common_substring.py
@@ -16,59 +16,69 @@ def solve(s, t):
 
 class CommonSubstring:
 	def __init__(self, s, t):
-		self.s = s
-		self.t = t
-		self.len_s = len(s)
-		self.len_t = len(t)
-		self._prime1 = 100007
-		self._prime2 = 100003
-		self._multiplier = 87
-		self.hash_s1 = self.hash_string(s, self._prime1)
-		self.hash_s2 = self.hash_string(s, self._prime2)
-		self.hash_t1 = self.hash_string(t, self._prime1)
-		self.hash_t2 = self.hash_string(t, self._prime2)
-		self.poly1 = self.precompute_poly(max(self.len_s, self.len_t)+1, self._prime1)
-		self.poly2 = self.precompute_poly(max(self.len_s, self.len_t)+1, self._prime2)
+		if len(s) <= len(t):
+			self.s = s
+			self.t = t
+			self.flag = True
+		else:
+			self.s = t
+			self.t = s
+			self.flag = False
+		self.len_s = len(self.s)
+		self.len_t = len(self.t)
+		self._prime1 = 1000000007
+		self._prime2 = 1000000009
+		self._multiplier = 113
+		self.hash_s1 = self.hash_string(self.s, self._prime1)
+		self.hash_s2 = self.hash_string(self.s, self._prime2)
+		self.hash_t1 = self.hash_string(self.t, self._prime1)
+		self.hash_t2 = self.hash_string(self.t, self._prime2)
+		self.poly1 = self.precompute_poly(self.len_t + 1, self._prime1)
+		self.poly2 = self.precompute_poly(self.len_t + 1, self._prime2)
 
 	def solve(self):
 		if self.s == self.t:
 			return Answer(0,0,self.len_s)
 		else:
-			k = min(self.len_s, self.len_t)
+			k = self.len_s
 			res = [None for _ in range(k+1)]
 			str_l1 = []
 			str_l2 = []
 			left = 0
 			right = k
-			k = (left + right) // 2
+			# k = (left + right) // 2
 			while k > 0:
-				table_s1 = self.precompute_hash(k, self.s, self.hash_s1, self._prime1, self.poly1)
-				table_s2 = self.precompute_hash(k, self.s, self.hash_s2, self._prime2, self.poly2)
-				table_t1 = self.precompute_hash(k, self.t, self.hash_t1, self._prime1, self.poly1)
-				table_t2 = self.precompute_hash(k, self.t, self.hash_t2, self._prime2, self.poly2)
-				match1 = self.match(table_s1, table_t1, k)
-				match2 = self.match(table_s2, table_t2, k)
+				hash_dict_s1 = self.precompute_hash(k, self.s, self.hash_s1, self._prime1, self.poly1)
+				hash_dict_s2 = self.precompute_hash(k, self.s, self.hash_s2, self._prime2, self.poly2)
+				match1 = self.match_hash(hash_dict_s1, k, self.t, self.hash_t1, self._prime1, self.poly1)
+				match2 = self.match_hash(hash_dict_s2, k, self.t, self.hash_t2, self._prime2, self.poly2)
 				ans = self.find(match1, match2)
-				if len(ans) != 0: #TODO
-					# return ans[0]
+				if len(ans) != 0:
 					res[k] = True
 					if k == min(self.len_s, self.len_t):
-						return ans[0]
-					elif res[k+1] == False:
-						return ans[0]
-					left = k
+						return ans[-1]
+					elif res[k + 1] == False:
+						return ans[-1]
+					left = k + 1
 					k = (left + right) // 2
 				else:
 					res[k] = False
-					right = k
+					right = k - 1
 					k = (left + right) // 2
 		return Answer(0,0,0)
 	
-	def match(self, l1, l2, k):
+	def match_hash(self, hash_dict, k, text, hash_l, _prime, poly):
 		res = []
-		for i in range(len(l1)):
-			if l1[i] in l2:
-				res.append(Answer(i,l2.index(l1[i]),k))
+		for i in range(0, len(text) - k + 1):
+			tmp = (hash_l[i + k] - poly[k] * hash_l[i]) % _prime
+			try:
+				pos = hash_dict[tmp]
+				if self.flag:
+					res.append(Answer(pos, i, k))
+				else:
+					res.append(Answer(i, pos, k))
+			except KeyError:
+				continue
 		return res
 
 	def find(self, l1, l2):
@@ -83,9 +93,9 @@ class CommonSubstring:
 		return hash_l
 	
 	def precompute_hash(self, k, text, hash_l, _prime, poly):
-		res = []
-		for i in range(0, len(text)-k+1):
-			res.append((hash_l[i+k]-poly[k]*hash_l[i])%_prime)
+		res = dict()
+		for i in range(0, len(text) - k + 1):
+			res.update({((hash_l[i + k] - poly[k] * hash_l[i]) % _prime) : i})
 		return res
 
 	def precompute_poly(self, l, _prime):
