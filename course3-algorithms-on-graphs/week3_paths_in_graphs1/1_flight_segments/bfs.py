5fd5719119ca144d4f8c0cd2fea4155f53ac4675
haydenz <40226873+haydenz@users.noreply.github.com>
2020-12-28 17:46:31 +0800

    BFS to find shortest distance

--- course3-algorithms-on-graphs/week3_paths_in_graphs1/1_flight_segments/bfs.py
+++ course3-algorithms-on-graphs/week3_paths_in_graphs1/1_flight_segments/bfs.py
@@ -3,9 +3,63 @@
 import sys
 import queue
 
-def distance(adj, s, t):
+
+class Vertex:
+    def __init__(self, key):
+        self.key = key
+        self.adj = []
+        self.visited = False
+
+class Graph:
+    def __init__(self, adj, n):
+        self.adj = adj
+        self.n = n
+        self.vertices = []
+        self.create_vertices()
+    
+    def create_vertices(self):
+        vertices = [None for _ in range(self.n)]
+        for i in range(self.n):
+            vertices[i] = Vertex(i)
+        for i in range(self.n):
+            for a in adj[i]:
+                vertices[i].adj.append(vertices[a])
+        self.vertices = vertices
+    
+    def BFS(self, s):
+        dist, prev = dict(), dict()
+        for u in self.vertices:
+            dist[u] = -1
+            prev[u] = None
+        dist[s] = 0
+        q = queue.Queue(maxsize=n)
+        q.put(s)
+        while not q.empty():
+            u = q.get()
+            for v in u.adj:
+                if dist[v] == -1:
+                    q.put(v)
+                    dist[v] = dist[u] + 1
+                    prev[v] = u
+        return prev, dist
+    
+    def reconstruct_path(self, s, u, prev):
+        result = []
+        while t != s:
+            result.append(u)
+            try:
+                t = prev[t]
+            except KeyError:
+                return []
+        return list(reversed(result))
+
+def distance(adj, n, s, t):
     #write your code here
-    return -1
+    graph = Graph(adj, n)
+    s = graph.vertices[s]
+    t = graph.vertices[t]
+    prev, dist = graph.BFS(s)
+    return dist[t]
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -18,4 +72,4 @@ if __name__ == '__main__':
         adj[a - 1].append(b - 1)
         adj[b - 1].append(a - 1)
     s, t = data[2 * m] - 1, data[2 * m + 1] - 1
-    print(distance(adj, s, t))
+    print(distance(adj, n, s, t))
