ca66789da0fae70b098afd2a744131d523a816a7
hayden <hayden.zhu@magnumwm.com>
2020-12-30 00:58:51 +0800

    TODO: compare two dists from dijkstra and bellman

--- course3-algorithms-on-graphs/week4_paths_in_graphs2/3_exchanging_money/shortest_paths.py
+++ course3-algorithms-on-graphs/week4_paths_in_graphs2/3_exchanging_money/shortest_paths.py
@@ -4,6 +4,64 @@ import sys
 import queue
 import math
 
+class Heap():
+    def __init__(self, data):
+        self.array, self.pos = self.create_array(data)
+        self.size = len(self.array)
+ 
+    def create_array(self, data):
+        arr, pos = [], []
+        for i, d in enumerate(data):
+            pos.append(i)
+            arr.append(d)
+        return arr, pos
+ 
+    def swap_node(self, a, b):
+        self.array[a], self.array[b] = self.array[b], self.array[a]
+ 
+    def heapify(self, idx): # raise up minimum node
+        smallest = idx
+        left = 2*idx + 1
+        right = 2*idx + 2
+        if left < self.size and self.array[left][1] < self.array[smallest][1]:
+            smallest = left
+        if right < self.size and self.array[right][1] < self.array[smallest][1]:
+            smallest = right
+        if smallest != idx:
+            self.pos[self.array[smallest][0]] = idx
+            self.pos[self.array[idx][0]] = smallest
+            self.swap_node(smallest, idx)
+            self.heapify(smallest)
+ 
+    def extract_min(self):
+        if self.is_empty() == True:
+            return
+        root = self.array[0]
+        last_node = self.array[self.size - 1]
+        self.array[0] = last_node
+        self.pos[last_node[0]] = 0
+        self.pos[root[0]] = self.size - 1
+        self.size -= 1
+        self.heapify(0)
+        return root
+ 
+    def is_empty(self):
+        return True if self.size == 0 else False
+ 
+    def decrease_key(self, v, dist):
+        i = self.pos[v]
+        self.array[i][1] = dist
+        while i > 0 and self.array[i][1] < self.array[(i - 1) // 2][1]:
+            self.pos[ self.array[i][0] ] = (i-1)//2
+            self.pos[ self.array[(i-1)//2][0] ] = i
+            self.swap_node(i, (i - 1)//2 )
+            i = (i - 1) // 2;
+ 
+    def in_heap(self, v):
+        if self.pos[v] < self.size:
+            return True
+        return False
+
 class Vertex:
     def __init__(self, key):
         self.key = key
@@ -77,6 +135,30 @@ class Graph:
             x = prev[x]
         
         return res
+    
+    def dijkstra(self, s):
+        dist, prev = dict(), dict()
+        for u in self.vertices:
+            dist[u], prev[u] = math.inf, None
+        dist[s] = 0
+
+        heap_l = [[u.key, dist[u]] for u in self.vertices]
+        h = Heap(heap_l)        
+        h.decrease_key(s.key, dist[s])
+
+        while not h.is_empty():
+            u = self.vertices[h.extract_min()[0]]
+            for v in u.adj:
+                if v == u:
+                    continue
+                v_dist_old = dist[v]
+                v_adj_idx = u.adj.index(v)
+                tmp = dist[u] + u.cost[v_adj_idx]
+                if dist[v] > tmp and dist[u] != math.inf and h.in_heap(v.key):
+                    dist[v] = tmp
+                    prev[v] = u
+                    h.decrease_key(v.key, dist[v])
+        return dist
 
 
 def shortest_paths(adj, cost, s, distance, reachable, shortest):
@@ -86,7 +168,7 @@ def shortest_paths(adj, cost, s, distance, reachable, shortest):
     flag, found, prev, dist = g.is_negative_cycle(source)
 
     # calculate distance
-    # dist = g.dijkstra(source)
+    dist = g.dijkstra(source)
     for node in dist:
         if dist[node] != math.inf:
             reachable[node.key] = 1
