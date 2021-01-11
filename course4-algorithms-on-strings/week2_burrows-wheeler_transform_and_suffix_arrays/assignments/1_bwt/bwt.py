f630f8ea5895b838a59beb8f539d66b144925be6
haydenz <haydenzhuxz@gmail.com>
2021-01-11 22:36:14 +0800

    finished: NaiveBWT, NaiveInvertBWT, and EfficientInvertBWT

--- course4-algorithms-on-strings/week2_burrows-wheeler_transform_and_suffix_arrays/assignments/1_bwt/bwt.py
+++ course4-algorithms-on-strings/week2_burrows-wheeler_transform_and_suffix_arrays/assignments/1_bwt/bwt.py
@@ -1,9 +1,19 @@
 # python3
 import sys
 
-def BWT(text):
-    return ""
+def NaiveBWT(text):
+    # Calculate all cyclic rotations
+    cyclic_rotations = []
+    for i in range(len(text)-1,-1,-1):
+        cyclic_rotations.append(text[i:] + text[:i])
+
+    # Extract the final char of sorted cyclic rotations
+    res = ""
+    for c in sorted(cyclic_rotations):
+        res += c[-1]
+
+    return res
 
 if __name__ == '__main__':
     text = sys.stdin.readline().strip()
-    print(BWT(text))
+    print(NaiveBWT(text))
--- course4-algorithms-on-strings/week2_burrows-wheeler_transform_and_suffix_arrays/assignments/2_bwtinverse/bwtinverse.py
+++ course4-algorithms-on-strings/week2_burrows-wheeler_transform_and_suffix_arrays/assignments/2_bwtinverse/bwtinverse.py
@@ -1,11 +1,27 @@
 # python3
 import sys
 
-def InverseBWT(bwt):
+def EfficientInverseBWT(bwt):
+    sorted_bwt = sorted([(b, i) for i, b in enumerate(bwt)])
+
+    k = bwt.index("$")
+    res = ""
+    for _ in range(len(bwt)):
+        t, k = sorted_bwt[k]
+        res += t
+    return res
+
+def NaiveInverseBWT(bwt):
     # write your code here
-    return ""
+    # Sort len(bwt) - 1 times
+    bwt_len = len(bwt)
+    first_cols = sorted(bwt)
+    for i in range(bwt_len-1):
+        first_cols = sorted([bwt[i] + first_cols[i] for i in range(bwt_len)])
+
+    return first_cols[0][1:] + "$"
 
 
 if __name__ == '__main__':
     bwt = sys.stdin.readline().strip()
-    print(InverseBWT(bwt))
+    print(EfficientInverseBWT(bwt))
